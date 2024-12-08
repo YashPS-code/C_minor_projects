0fdb5500f7749abd32ac4c5b8539aa0514ae81f0
yashps-code <ypsingh345612@yahoo.com>
2024-12-08 14:27:56 +0530

    Stone Paper Scissor game in c program Project

---
+++ help.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int game(char ch){
+    char str[3]={'s','p','r'};
+    int compChoice = rand()%3;
+    char result = str[compChoice];
+    if(ch==result){
+        printf("tie\n");
+    }else if(ch=='s' && result=='p' || ch=='p' && result=='r' || ch=='r' && result=='s'){
+        printf("Computer Wins\n");
+    }else if(ch=='p' && result=='s' || ch=='r' && result=='p' || ch=='s' && result=='r'){
+        printf("User Wins\n");
+    }else{
+        printf("Invalid Input\n");
+    }
+    return 0;
+}
+
+int main(){
+    char userChoice;
+    int (*fptr)(char);
+    fptr = &game;
+    printf("s:Stone\np:Paper\nr:Scissor\n");
+    while(true){
+        printf("Enter your Choice: ");
+        scanf(" %c",&userChoice);
+        /*if(userChoice != 's' || userChoice !='p'|| userChoice != 'r'){
+            break;
+        }*/
+        fptr(userChoice);
+    }
+    return 0;
+}
