a826296281f88791007d9a970f93bc5fd02644de
yashps-code <ypsingh345612@yahoo.com>
2024-12-09 11:52:38 +0530

    hangman game with C

--- hangman_game.c
+++ hangman_game.c
@@ -3,7 +3,7 @@
 #include <time.h>
 #include <string.h>
 
-int hangman(){
+int hangman(char *ptr){
     srand(time(NULL));
     char *wordList[4][2] = {
         { "geeksforgeeks", "Computer coding" },
@@ -18,22 +18,32 @@ int hangman(){
     int extrudeSizeMax = (1/2)*wordLength;
     int extrudeSizeMin = (2/5)*wordLength;
     int extrudeSize = rand()%(extrudeSizeMax-extrudeSizeMin+1)+extrudeSizeMin;
+    int extrudingArr[] ={};
     for(int i=0;i<extrudeSize;i++){
-        int extrudeingArr[] ={};
         int extrudePos = rand()%wordLength;
+        int flag= 1;
         for(int j=1;j<=i;j++){
-            if(extrudePos){
-                return 0;
+            if(extrudePos==extrudingArr[j]){
+                flag = 0;
             }
-
+        }
+        if(flag == 0){
+            continue;
+        }else{
+            extrudingArr[i]=extrudePos;
         }
     }
-
+    for(int i=0;i<extrudeSize;i++){
+        wordGot[extrudingArr[i]]=' ';
+    } 
+    *ptr =wordGot;
     return 0;
 }
 
 int main(){
-    hangman();
+    char word[50];
+    int ptr = &word;
+    hangman(ptr);
 
     return 0;
 }
