67f677e710253723527c83ead90e9a00a507df0d
yashps-code <ypsingh345612@yahoo.com>
2024-12-09 16:43:50 +0530

    minor error fixes

--- hangman_game.c
+++ hangman_game.c
@@ -12,38 +12,44 @@ int hangman(char *ptr){
         { "beach", "Sandy shore by the sea" },
     };
     int wordPick = rand()%4;
-    char wordGot[50];
-    strcpy(wordGot,wordList[wordPick][0]);
-    int wordLength = sizeof(wordGot);
-    int extrudeSizeMax = (1/2)*wordLength;
-    int extrudeSizeMin = (2/5)*wordLength;
+    char *wordGot = wordList[wordPick][0];
+    int wordLength = strlen(wordGot);
+    int extrudeSizeMax = 0.5*wordLength;
+    int extrudeSizeMin = 0.4*wordLength;
     int extrudeSize = rand()%(extrudeSizeMax-extrudeSizeMin+1)+extrudeSizeMin;
+    printf("%d\n",extrudeSize);
     int extrudingArr[] ={};
     for(int i=0;i<extrudeSize;i++){
-        int extrudePos = rand()%wordLength;
+        int extrudePos= (rand()%wordLength);
         int flag= 1;
-        for(int j=1;j<=i;j++){
+        for(int j=0;j<i;j++){
             if(extrudePos==extrudingArr[j]){
                 flag = 0;
             }
         }
-        if(flag == 0){
+        if(flag == 0 || extrudePos > wordLength){
             continue;
         }else{
             extrudingArr[i]=extrudePos;
         }
     }
+    for(int i=0;i<extrudeSize;i++)
+    printf("%d\n",extrudingArr[i]);
+
     for(int i=0;i<extrudeSize;i++){
+        //printf("%c",wordGot[extrudingArr[i]]);
         wordGot[extrudingArr[i]]=' ';
-    } 
-    *ptr =wordGot;
+    }
+    //printf("%s",wordGot);
+    ptr =wordGot;
     return 0;
 }
 
 int main(){
     char word[50];
-    int ptr = &word;
+    char *ptr = word;
     hangman(ptr);
+    //printf("%s",word);
 
     return 0;
 }
