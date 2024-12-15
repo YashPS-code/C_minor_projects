85336bf43b162beb64945c6c8d4560d1175e2669
yashps-code <ypsingh345612@yahoo.com>
2024-12-15 21:22:32 +0530

    main function coding completion

--- hangman_game.c
+++ hangman_game.c
@@ -3,7 +3,14 @@
 #include <time.h>
 #include <string.h>
 
-int hangman(char *ptr){
+#define MaxTry = 6;
+
+struct store{
+        int num;
+        char chr;
+    };
+
+int hangman(char *word,char *hint, struct store *answerCharacter){
     srand(time(NULL));
     char *wordList[4][2] = {
         { "geeksforgeeks", "Computer coding" },
@@ -14,8 +21,8 @@ int hangman(char *ptr){
     int wordPick = rand()%4;
     char *wordGot = wordList[wordPick][0];
     const int wordLength = strlen(wordGot);
-    int extrudeSizeMax = 0.5*wordLength;
-    int extrudeSizeMin = 0.4*wordLength;
+    int extrudeSizeMax = 0.9*wordLength;
+    int extrudeSizeMin = 0.5*wordLength;
     int extrudeSize = rand()%(extrudeSizeMax-extrudeSizeMin+1)+extrudeSizeMin;
     int extrudingArr[10] = {};
     for(int i=0;i<extrudeSize;i++){
@@ -37,21 +44,45 @@ int hangman(char *ptr){
 
     char newString[50];
 
-    for(int i=0;i<wordLength;i++){
+    for(int i=0;i<=wordLength;i++){
         newString[i]=wordGot[i];
     }
-    
+
     for(int i=0;i<extrudeSize;i++){
+        answerCharacter->num = extrudingArr[i];
+        answerCharacter->chr = newString[extrudingArr[i]];
         newString[extrudingArr[i]]='_';
+    }
+
+    strcpy(word,newString);
+    strcpy(hint,wordList[wordPick][1]);
+    return 0;
+}
+
+int inputCheck(char *work, int *pass)
+
+int main(){
+    char word[50];
+    char hint[50],userInp;
+    int *passStage, userTry = 1;
+    struct store* answerCharacter;
+    hangman(word,hint,answerCharacter);
+    while(userTry<MaxTry){
+        printf("%s\t\t\t%s",word,hint);
+        scanf(" %c",&userInp);
+        inputCheck(word,passStage,userInp,/*extrude array*/);
+        if(passStage==0){
+            userTry++;
+            if(userTry==MaxTry){
+                printf("All tries Exhausted.");
+                break;
+            }
+        }
+
     }
     return 0;
 }
 
-int main(){
-    char word[50];
-    char *ptr = word;
-    hangman(ptr);
-    printf("%s",word);
-
-    return 0;
+int inputCheck(char *word, int *pass, char userInp, /*Another ...*/){
+    
 }
